aa42b3d9ec5966e01255418545e8521608fa9435
Sami <sami.vuorenheimo@gmail.com>
2023-05-31 18:22:51 +0300

    changed variables to objects and remade app to function

--- part1/course_information/src/App.js
+++ part1/course_information/src/App.js
@@ -1,27 +1,42 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
 
   const Header = ( { course } ) => <h1>{ course }</h1>
 
 
+  const Part = ({ part }) => {
+    return (
+      <p>{ part.name } { part.exercises }</p>
+    )
+  }
+
+
   const Content = ( { parts }) => {
     return (
-          <>
-            {parts.map(part => <p>{ part }</p>)}
-          </>
+          <div>
+            <Part part={ parts[0] } />
+            <Part part={ parts[1] } />
+            <Part part={ parts[2] } />
+          </div>
       )
     }
 
 
   const Total = ({ exercises }) => {
-    const sum = exercises.reduce((a, b) => a + b, 0)
+    const sum = exercises.reduce((accumulator, value) => value.exercises + accumulator, 0)
 
     return (
       <p>Number of exercises { sum }</p>
@@ -32,7 +47,7 @@ const App = () => {
     <div>
       <Header course={ course } />
       <Content parts={ [part1, part2, part3] } />
-      <Total exercises={ [exercises1, exercises2, exercises3] } />
+      <Total exercises={ [part1, part2, part3] } />
     </div>
   )
 }
