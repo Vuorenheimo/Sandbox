182822a3005fc3adcd4976ab252bf9cebbee4c4f
Sami <sami.vuorenheimo@gmail.com>
2023-06-06 10:55:42 +0300

    adjusted some things and made StatisticLine component

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,6 +1,39 @@
 import { useState } from 'react'
 
 
+const Header = (props) => <h1>{props.headline}</h1>
+
+
+const Button = (props) => <button onClick={props.onClick}>{props.text}</button>
+
+
+const StatisticsLine = (props) => <div>{props.text} {props.value}</div>
+
+
+const Statistics = (props) => {
+
+  const all = props.good + props.bad + props.neutral
+  const average = all === 0 ? 0 : (props.good - props.bad) / all
+  const positive = all === 0 ? 0 : props.good / all
+
+  if (all === 0) {
+    return <p>No feedback given</p>
+  }
+
+  return (
+    <div>
+      <Header headline={ props.headline } />
+      <StatisticsLine text={ "Good" } value={ props.good } />
+      <StatisticsLine text={ "Neutral" } value={ props.neural } />
+      <StatisticsLine text={ "Bad" } value={ props.bad } />
+      <StatisticsLine text={ "All" } value={ all } />
+      <StatisticsLine text={ "Average" } value={ average } />
+      <StatisticsLine text={ "Positive" } value={ positive } />
+    </div>
+  )
+}
+
+
 const App = () => {
 
   const [good, setGood] = useState(0)
@@ -8,40 +41,13 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
 
-  const Header = (props) => <h1>{props.headline}</h1>
-
-
-  const Button = (props) => <button onClick={props.onClick}>{props.text}</button>
-
-
-  const Statistics = (props) => {
-
-    const all = props.good + props.bad + props.neutral
-    const average = all === 0 ? 0 : (props.good - props.bad) / all
-    const positive = all === 0 ? 0 : props.good / all
-
-
-    return (
-      <div>
-        <Header headline={props.headline} />
-        <div>good {props.good}</div>
-        <div>neutral {props.neutral}</div>
-        <div>bad {props.bad}</div>
-        <div>all {all}</div>
-        <div>average {average}</div>
-        <div>positive {positive}</div>
-      </div>
-    )
-  }
-
-
   return (
     <div>
-      <Header headline={ "give feedback" } />
-      <Button text={ "good" } onClick={() => setGood(good + 1)} />
-      <Button text={ "neutral" } onClick={() => setNeutral(neutral + 1)} />
-      <Button text={ "bad" } onClick={() => setBad(bad + 1)} />
-      <Statistics headline={ "statistics" } good={ good } neutral={ neutral } bad={ bad } />
+      <Header headline={ "Give feedback" } />
+      <Button text={ "Good" } onClick={() => setGood(good + 1)} />
+      <Button text={ "Neutral" } onClick={() => setNeutral(neutral + 1)} />
+      <Button text={ "Bad" } onClick={() => setBad(bad + 1)} />
+      <Statistics headline={ "Statistics" } good={ good } neutral={ neutral } bad={ bad } />
     </div>
   )
 }
