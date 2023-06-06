f187312ed1466e4ef27fc18775d8abbf7a5339c4
Sami <sami.vuorenheimo@gmail.com>
2023-06-06 10:41:24 +0300

    created calculations for average and positive

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,13 +2,10 @@ import { useState } from 'react'
 
 
 const App = () => {
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const [all, setAll] = useState(0)
-  const [average, setAverage] = useState(0)
-  const [positive, setPositive] = useState(0)
 
 
   const Header = (props) => <h1>{props.headline}</h1>
@@ -19,15 +16,20 @@ const App = () => {
 
   const Statistics = (props) => {
 
+    const all = props.good + props.bad + props.neutral
+    const average = all === 0 ? 0 : (props.good - props.bad) / all
+    const positive = all === 0 ? 0 : props.good / all
+
+
     return (
       <div>
         <Header headline={props.headline} />
         <div>good {props.good}</div>
         <div>neutral {props.neutral}</div>
         <div>bad {props.bad}</div>
-        <div>all {props.all}</div>
-        <div>average {props.average}</div>
-        <div>positive {props.positive}</div>
+        <div>all {all}</div>
+        <div>average {average}</div>
+        <div>positive {positive}</div>
       </div>
     )
   }
@@ -39,8 +41,7 @@ const App = () => {
       <Button text={ "good" } onClick={() => setGood(good + 1)} />
       <Button text={ "neutral" } onClick={() => setNeutral(neutral + 1)} />
       <Button text={ "bad" } onClick={() => setBad(bad + 1)} />
-      <Statistics headline={ "statistics" } good={ good } neutral={ neutral } bad={ bad }
-        all={ all } average={ average } positive={ positive } />
+      <Statistics headline={ "statistics" } good={ good } neutral={ neutral } bad={ bad } />
     </div>
   )
 }
